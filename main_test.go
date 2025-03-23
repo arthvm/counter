3bf07eb12d22e3c9df33679275a270bd0ee2e8b3
Arthur Mariano <arthvm@proton.me>
2025-03-23 18:13:23 -0300

    feat: add basic edge cases test

--- main_test.go
+++ main_test.go
@@ -6,9 +6,30 @@ func TestCountWords(t *testing.T) {
 	input := "one two three four five"
 	wants := 5
 
-	res := countWords([]byte(input))
+	res := CountWords([]byte(input))
 
 	if res != wants {
+		t.Logf("expected: %d got: %d", wants, res)
+		t.Fail()
+	}
+
+	input = ""
+	wants = 0
+
+	res = CountWords([]byte(input))
+
+	if res != wants {
+		t.Logf("expected: %d got: %d", wants, res)
+		t.Fail()
+	}
+
+	input = " "
+	wants = 0
+
+	res = CountWords([]byte(input))
+
+	if res != wants {
+		t.Logf("expected: %d got: %d", wants, res)
 		t.Fail()
 	}
 }
