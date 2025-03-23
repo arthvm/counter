c93db1877e251b34385be0be70d9d8b7a15cb6c6
Arthur Mariano <arthvm@proton.me>
2025-03-23 19:31:46 -0300

    feat: add read error handling with stderr

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	data, _ := os.ReadFile("./words.txt")
+	filename := "./noexists.txt"
+
+	log.SetFlags(0)
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("failed to read file:", err)
+	}
 
 	wordCount := CountWords(data)
 
