eedda27c4a3f823c66b74d766b1d4ad9ad486c0f
Arthur Mariano <arthvm@proton.me>
2025-03-24 23:48:56 -0300

    feat: improve counting algorithm with buffers

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"unicode"
 )
 
 func main() {
-	filename := "./lots_of_words.txt"
+	filename := "./words.txt"
 
 	log.SetFlags(0)
 
@@ -17,28 +18,36 @@ func main() {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	PrintFileContents(file)
-
-	// wordCount := CountWords(data)
-	//
-	// fmt.Println(wordCount)
+	wordCount := CountWordsInFile(file)
+	fmt.Println(wordCount)
 }
 
-func PrintFileContents(file *os.File) {
+func CountWordsInFile(file *os.File) int {
+	wordCount := 0
+	isInsideWord := false
+
 	const bufferSize = 4096 * 2
 	buffer := make([]byte, bufferSize)
 
-	totalSize := 0
 	for {
 		size, err := file.Read(buffer)
 		if err != nil {
 			break
 		}
 
-		totalSize += size
+		isInsideWord = !unicode.IsSpace(rune(buffer[0])) && isInsideWord
+
+		bufferCount := CountWords(buffer[:size])
+		if isInsideWord {
+			bufferCount -= 1
+		}
+
+		wordCount += bufferCount
+
+		isInsideWord = !unicode.IsSpace(rune(buffer[size-1]))
 	}
 
-	fmt.Println("total bytes read:", totalSize)
+	return wordCount
 }
 
 func CountWords(data []byte) int {
