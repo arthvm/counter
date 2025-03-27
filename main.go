1ad1345f4b8e385e51e7446fc5483cff3e4acaa0
Arthur Mariano <arthvm@proton.me>
2025-03-26 22:45:03 -0300

    chore: separate code on new files for refactor and new features

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
@@ -40,28 +38,4 @@ func main() {
 	if didError {
 		os.Exit(1)
 	}
-}
-
-func CountWordsInFile(filename string) (int, error) {
-	file, err := os.Open(filename)
-	defer file.Close()
-
-	if err != nil {
-		return 0, err
-	}
-
-	return CountWords(file), nil
-}
-
-func CountWords(file io.Reader) int {
-	wordCount := 0
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanWords)
-
-	for scanner.Scan() {
-		wordCount++
-	}
-
-	return wordCount
 }
